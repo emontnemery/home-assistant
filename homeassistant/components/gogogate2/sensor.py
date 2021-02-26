8201fcfc4e23b34993443f88324dbd8af11824c6
Erik <erik@montnemery.com>
2021-02-26 14:01:23 +0100

    tmp

--- homeassistant/components/gogogate2/sensor.py
+++ homeassistant/components/gogogate2/sensor.py
@@ -22,6 +22,7 @@ from homeassistant.const import (
     CONF_IP_ADDRESS,
     CONF_PASSWORD,
     CONF_USERNAME,
+    DEVICE_CLASS_BATTERY,
 )
 from homeassistant.core import HomeAssistant
 import homeassistant.helpers.config_validation as cv
@@ -35,29 +36,6 @@ from .common import (
 )
 from .const import DEVICE_TYPE_GOGOGATE2, DEVICE_TYPE_ISMARTGATE, DOMAIN, MANUFACTURER
 
-#TODO: Where is this schema used?
-COVER_SCHEMA = vol.Schema(
-    {
-        vol.Required(CONF_IP_ADDRESS): cv.string,
-        vol.Required(CONF_DEVICE, default=DEVICE_TYPE_GOGOGATE2): vol.In(
-            (DEVICE_TYPE_GOGOGATE2, DEVICE_TYPE_ISMARTGATE)
-        ),
-        vol.Required(CONF_PASSWORD): cv.string,
-        vol.Required(CONF_USERNAME): cv.string,
-    }
-)
-
-
-async def async_setup_platform(
-    hass: HomeAssistant, config: dict, add_entities: Callable, discovery_info=None
-) -> None:
-    """Convert old style file configs to new style configs."""
-    hass.async_create_task(
-        hass.config_entries.flow.async_init(
-            DOMAIN, context={"source": SOURCE_IMPORT}, data=config
-        )
-    )
-
 
 async def async_setup_entry(
     hass: HomeAssistant,
@@ -69,14 +47,14 @@ async def async_setup_entry(
 
     async_add_entities(
         [
-            DeviceCover(config_entry, data_update_coordinator, door)
-            for door in get_configured_doors(data_update_coordinator.data)
+            DoorSensor(config_entry, data_update_coordinator, door)
+            for door in get_configured_doors(data_update_coordinator.data) if door.sensorid
         ]
     )
 
 
-class DeviceCover(CoordinatorEntity, CoverEntity):
-    """Cover entity for goggate2."""
+class DoorSensor(CoordinatorEntity):
+    """Sensor entity for goggate2."""
 
     def __init__(
         self,
@@ -100,48 +78,24 @@ class DeviceCover(CoordinatorEntity, CoverEntity):
     @property
     def name(self):
         """Return the name of the door."""
-        return self._get_door().name
-
-    @property
-    def is_closed(self):
-        """Return true if cover is closed, else False."""
-        door = self._get_door()
-
-        if door.status == DoorStatus.OPENED:
-            return False
-        if door.status == DoorStatus.CLOSED:
-            return True
-
-        return None
+        return f"{self._get_door().name} sensor"
 
     @property
     def device_class(self):
         """Return the class of this device, from component DEVICE_CLASSES."""
-        door = self._get_door()
-        if door.gate:
-            return DEVICE_CLASS_GATE
-
-        return DEVICE_CLASS_GARAGE
+        return DEVICE_CLASS_BATTERY
 
     @property
-    def supported_features(self):
-        """Flag supported features."""
-        return SUPPORT_OPEN | SUPPORT_CLOSE
-
-    async def async_open_cover(self, **kwargs):
-        """Open the door."""
-        await self._api.async_open_door(self._get_door().door_id)
-
-    async def async_close_cover(self, **kwargs):
-        """Close the door."""
-        await self._api.async_close_door(self._get_door().door_id)
+    def state(self):
+        """Return the state of the entity."""
+        door = self._get_door()
+        return door.voltage
 
     @property
     def state_attributes(self):
         """Return the state attributes."""
         attrs = super().state_attributes
         door = self._get_door()
-        attrs["door_id"] = door.door_id
         if door.sensorid is not None:
             attrs["sensorid"] = door.door_id
         return attrs
@@ -153,12 +107,7 @@ class DeviceCover(CoordinatorEntity, CoverEntity):
 
     @property
     def device_info(self):
-        """Device info for the controller."""
-        data = self.coordinator.data
+        """Device info for the controller, details are set by the cover entity."""
         return {
             "identifiers": {(DOMAIN, self._config_entry.unique_id)},
-            "name": self._config_entry.title,
-            "manufacturer": MANUFACTURER,
-            "model": data.model,
-            "sw_version": data.firmwareversion,
         }
